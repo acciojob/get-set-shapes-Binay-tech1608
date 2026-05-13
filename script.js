b934998c5aee83102d2f654800a687008efb0300
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-05-13 13:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width = width;
+		this._height = height;
+	}
+	get width(){
+		this._width = width;
+	}
+	get height(){
+		this._height = height;
+	}
+	getArea(){
+		return this.width * this.height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		super(width, height);
+		this._width = side;
+		this._height = side;
+		this.side = side;
+	}
+	get side(){
+		this._side = side;
+	}
+	getPerimeter(){
+		return (4 * this.side);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
